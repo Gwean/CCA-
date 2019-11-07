848873060140f04de8a38702d8cee8f9452be839
Gwean <georgina.mondino1@gmail.com>
2019-11-07 12:28:49 -0300

    Mostrar y Ocultar otro en checkbox

--- Scripts/Formulario.js
+++ Scripts/Formulario.js
@@ -1,4 +1,20 @@
-function MostrarOtro(divId, element)
-{
+function MostrarOtroSelect(divId, element) {
     document.getElementById(divId).style.display = element.value == "Otro" ? 'block' : 'none';
+}
+function MostrarOtroCheckbox(divId, element) {
+    document.getElementById(divId).style.display = element.checked == true ? 'block' : 'none';
+  }
+
+
+function Clickeado(elementId) {
+    if (document.getElementById(elementId).value == "0") {
+        document.getElementById(elementId).classList.remove('FormularioEspacioBoton');
+        document.getElementById(elementId).classList.add('FormularioEspacioBotonActivo');
+        document.getElementById(elementId).value = "1";
+
+    } else {
+        document.getElementById(elementId).classList.remove('FormularioEspacioBotonActivo');
+        document.getElementById(elementId).classList.add('FormularioEspacioBoton');
+        document.getElementById(elementId).value = "0";
+    }
 }
